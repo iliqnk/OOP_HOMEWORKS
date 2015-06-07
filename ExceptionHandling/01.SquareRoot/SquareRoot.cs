b4dfe06cee6234953fa416701d3e97ff4b0253bf
iliqnk <iliqnkolev@gmail.com>
2015-06-07 15:37:00 +0300

    Added Exception Handling Homework

---
+++ ExceptionHandling/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException("number", "Number can't be negative.");
+                }
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine("Square root of {0} is {1}", number, squareRoot);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
